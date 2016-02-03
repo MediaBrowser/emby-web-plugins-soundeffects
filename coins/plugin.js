51a4f336cff013cacdba0caf7dd5674799b7dace
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-02-03 01:49:11 -0500

    initial commit

---
+++ coins/plugin.js
@@ -0,0 +1,18 @@
+define([], function () {
+
+    return function () {
+
+        var self = this;
+
+        self.name = 'Coins';
+        self.type = 'soundeffects';
+        self.id = 'coinsoundeffects';
+
+        self.getEffects = function () {
+            return {
+                navigation: 'navigation.wav',
+                select: 'select.wav'
+            };
+        };
+    }
+});
